5ca2b8da72592c54ce42ba9bbebb0287cbcb041a
Priyabashu Ambedkar <priyabashukgg@gmail.com>
2025-06-04 23:23:06 +0530

    Added a message on home page

--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,8 @@ const app = express();
 app.use(express.json());
 app.use('/identify', identifyRoute);
 
+app.get("/", (req, res) => {
+  res.send("Bitespeed Identity Reconciliation API is running ✅");
+});
+
 export default app;
